e2010fc7c2bb95c3bf07feca041a2a5640f9fc81
Laura Dunca <laura.dunca@evozon.com>
2021-08-25 17:38:19 +0300

    started working on cards

---
+++ trello/Tests/CardTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using trello.Helpers;
+using trello.Helpers.clients;
+using trello.Helpers.models;
+
+namespace trello.Tests
+{
+    [TestClass]
+    public class CardTest
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            var client = new RestClient(Constants.BaseUrl);
+            BoardClient.DeleteBoards(client);
+            var response = BoardClient.CreateBoard(client);
+            var board = JsonConvert.DeserializeObject<Board>(response.Content);
+            ListClient.CreateList(client, board.Id, Faker.Name.FullName());
+            //CardClient.CreateCard(...);
+        }
+
+        // TODO: CardClient class
+        //       Tests for Create, Get, Update, Delete Card
+    }
+}
